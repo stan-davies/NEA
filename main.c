c223a47de3a9669a089cbff3648d046d228354ff
Stan Davies <stan07davies@gmail.com>
2024-11-13 19:25:01 +0000

    added basic interface

--- main.c
+++ main.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "version.h"
 
+enum CMD {
+        REND,
+        EDIT,
+        NONE
+};
+
 int main(int argc, char **argv) {
-        printf("this is going to be something cool someday!\n");
-        printf("running version %s\n", VERSION);
+        printf("running at version %s\n", VERSION);
+
+        if (argc <= 1) {
+                printf("ERROR: no command given\n");
+                return 0;
+        }
+
+        char *cmd_str = argv[1];
+        enum CMD cmd = NONE;
+
+        if (0 == strcmp(cmd_str, "render")) {
+                cmd = REND;
+        } else if (0 == strcmp(cmd_str, "edit")) {
+                cmd = EDIT;
+        } else {
+                printf("ERROR: invalid command given at symbol '%s'", cmd_str);
+                return 0;
+        }
+
+        switch(cmd) {
+        case REND:
+                printf("you have chosen to render something\n");
+                break;
+        case EDIT:
+                printf("you have chosen to edit something\n");
+                break;
+        default:
+                break;
+        }
 
         return 0;
 }
